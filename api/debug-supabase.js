aaf2b8988344a76854b9a9dd6aebca40791067c8
traveltime07-hash <traveltime07@gmail.com>
2025-08-21 16:25:24 +0200

    Update debug-supabase.js

--- api/debug-supabase.js
+++ api/debug-supabase.js
@@ -1,4 +1,9 @@
-import { supabase } from "../src/supabase.js";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
 export default async function handler(req, res) {
   try {
